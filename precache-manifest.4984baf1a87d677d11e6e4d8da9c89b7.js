f3899687d27b62f12c65a78783944a8d02a24d49
Pere <pmanresa93@gmail.com>
2019-08-08 13:10:13 +0000

    Automated deployment to GitHub Pages: 7bce18b70c44f216d10068ba813d14c9198e1096

---
+++ precache-manifest.4984baf1a87d677d11e6e4d8da9c89b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d0805009aa39c37f1010",
+    "url": "/static/js/main.0f50cf15.chunk.js"
+  },
+  {
+    "revision": "8ff1ba6837c03449f869",
+    "url": "/static/js/2.6de30648.chunk.js"
+  },
+  {
+    "revision": "d0805009aa39c37f1010",
+    "url": "/static/css/main.6f9d2b08.chunk.css"
+  },
+  {
+    "revision": "8ff1ba6837c03449f869",
+    "url": "/static/css/2.24da6f85.chunk.css"
+  },
+  {
+    "revision": "14977b9f33edb56df719a75f1c5bdcdb",
+    "url": "/index.html"
+  }
+];
